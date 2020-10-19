142be7a21937296070e610aa311f1aea689c17e6
martin <martinconcetti97@gmail.com>
2020-10-18 21:06:15 -0300

    Basic main menu

---
+++ Fly or fall/Assets/Scrips/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+   public void PlayGame()
+    {
+       SceneManager.LoadScene("Game");
+       Debug.Log("Play with 1 player");
+
+    }
+}
+
+
